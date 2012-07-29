0c89abbb28bbb902b3461cd9a3ee5ae4823aa8ff
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-29 18:25:50 +0300

    initial commit

---
+++ src/TwitterDoodle/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("TwitterDoodle")]
+[assembly: AssemblyDescription("TwitterDoodle is fully asynchronous .NET Twitter client which embraces Task Parallel Library from top to bottom")]
